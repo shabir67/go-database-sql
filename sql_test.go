84cfd9291ee1cd7920a7d5a6483e1a7b2b758428
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MBP.lan>
2023-06-04 20:30:07 +0700

    added: query Select with "QueryContext"

--- sql_test.go
+++ sql_test.go
@@ -12,11 +12,36 @@ func TestExecSql(t *testing.T) {
 
 	ctx := context.Background()
 
-	query := "INSERT INTO customer(id, name) VALUES('shobir','Shobir')"
+	query := "INSERT INTO customer(id, name) VALUES('alya','Alya')"
 	_, err := db.ExecContext(ctx, query)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+	}
 }
