35ca5d8d6ca2799aef4bfbac4cb44a72a620f940
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MBP.lan>
2023-06-04 19:03:50 +0700

    implemented: databaseconnection

--- database_test.go
+++ database_test.go
@@ -1,11 +1,22 @@
 package godatabase
 
 import (
+	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func TestEmpty(t *testing.T) {
+
+}
+
+func TestOpemConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
 
 }
