b0720801048cddcd45146fc670f81101235d8ac6
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-06-05 19:00:57 +0700

    implemented:db commit and rollback

--- sql_test.go
+++ sql_test.go
@@ -221,3 +221,38 @@ func TestPrepareStatement(t *testing.T) {
 		fmt.Println("Comment Id ", id)
 	}
 }
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	sqlQuery := "INSERT INTO comments(email, comment) VALUES(?,?)"
+	// db transaction
+	for i := 0; i < 10; i++ {
+		email := "joko" + strconv.Itoa(i) + "gmail.com"
+		comment := "komentar ke " + strconv.Itoa(i)
+
+		result, err := tx.ExecContext(ctx, sqlQuery, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment Id", id)
+	}
+
+	err = tx.Rollback()
+	if err != nil {
+		panic(err)
+	}
+}
